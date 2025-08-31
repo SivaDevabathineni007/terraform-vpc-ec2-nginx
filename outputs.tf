3c3965efca49c95e6dfc127472b3cbc41d5fc4ac
Siva Devabathineni <sivanenii007@gmail.com>
2025-08-31 11:15:46 +0530

    wire(instance): launch public EC2 in public subnet and output public IP/URL

---
+++ outputs.tf
@@ -0,0 +1,20 @@
+output "vpc_id" {
+  value = module.network.vpc_id
+}
+
+output "public_subnet_ids" {
+  value = module.network.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  value = module.network.private_subnet_ids
+}
+
+output "instance_public_ip" {
+  value = module.web.public_ip
+}
+
+output "http_test_url" {
+  value = "http://${module.web.public_ip}"
+}
+
